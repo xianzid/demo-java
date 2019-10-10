c7501dcf4990568c72e18d629e6848a61de0bbe6
hongbiangoal <405691904@qq.com>
2019-10-10 11:33:53 +0800

    加空格

--- src/main/java/com/example/demo/test/reflect/BaseReflectMethod.java
+++ src/main/java/com/example/demo/test/reflect/BaseReflectMethod.java
@@ -8,11 +8,14 @@ import com.example.demo.test.reflect.object.Dept;
 public class BaseReflectMethod {
     public static void main(String[] args) {
         Class aClass;
+
         //1.通过对象的getClass()方法
         Dept dept = new Dept();
         aClass = dept.getClass();
+
         //2.通过对象的class属性
         aClass = Dept.class;
+
         //3.通过Class的forName()方法
         try {
             aClass = Class.forName("Dept");
