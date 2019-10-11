78832707237762027c312a502c00d63a8d1c5ac5
hongbiangoal <405691904@qq.com>
2019-10-11 10:49:13 +0800

    反射实现了面向接口的概念，同样的功能用接口实现，与工厂模式一样 客户端不关心对象从哪里来，只关心能不能取到对象，不关心子类

--- src/main/java/com/example/demo/test/reflect/ToAnInterface.java
+++ src/main/java/com/example/demo/test/reflect/ToAnInterface.java
@@ -2,6 +2,7 @@ package com.example.demo.test.reflect;
 
 /**
  * 反射实现了面向接口的概念，同样的功能用接口实现，与工厂模式一样
+ * 客户端不关心对象从哪里来，只关心能不能取到对象，不关心子类
  */
 public class ToAnInterface {
     public static void main(String[] args) {
@@ -13,24 +14,27 @@ public class ToAnInterface {
      * 反射的实现
      */
     private static void reflectImp(){
-        new UserFactory().getUser("com.example.demo.test.reflect.Customer");
+        final User user = new UserFactory().getInstance("com.example.demo.test.reflect.Customer");
+        user.getInfo();
     }
 
     /**
      * 简单工厂模式的实现
      */
     private static void factoryImp(){
-        new UserFactory().getUser(UserFactory.CMP);
+        //客户端不关心对象从哪里来，只关心能不能取到对象，不关心子类
+        final User user = new UserFactory().getInstance(UserFactory.CMP);
+        user.getInfo();
     }
 }
 
 class UserFactory{
     final static int CUS = 1;
     final static int CMP = 2;
-    final static int PLATFORM = 2;
-    final static int AGENT = 2;
+    final static int PLATFORM = 3;
+    final static int AGENT = 4;
 
-    public User getUser(String className){
+    public User getInstance(String className){
         User user = null;
         final Class<?> aClass;
         try {
@@ -42,7 +46,7 @@ class UserFactory{
         return user;
     }
 
-    public User getUser(int type){
+    public User getInstance(int type){
         User user = null;
         if (CUS == type){
             return new Customer();
@@ -57,24 +61,30 @@ class UserFactory{
     }
 }
 
-interface User{ }
+interface User{
+    public void getInfo();
+}
 class Customer implements User{
-    Customer(){
+    @Override
+    public void getInfo() {
         System.out.println("我是用户");
     }
 }
 class InsuranceCmy implements User{
-    InsuranceCmy(){
+    @Override
+    public void getInfo() {
         System.out.println("我是保险公司人员");
     }
 }
 class Platform implements User{
-    Platform(){
+    @Override
+    public void getInfo() {
         System.out.println("我是平台人员");
     }
 }
 class Agent implements User{
-    Agent(){
+    @Override
+    public void getInfo() {
         System.out.println("我是推广代理人员");
     }
 }
