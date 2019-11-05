35ccec07518a98cbf8c32b046f5475129d991ac6
hongbiangoal <405691904@qq.com>
2019-11-05 14:35:00 +0800

    网络基础-序列化和反序列化的实现：利用序列化和反序列化实现了深克隆

--- src/main/java/com/example/demo/test/socket/serializer/clone/CloneTest.java
+++ src/main/java/com/example/demo/test/socket/serializer/clone/CloneTest.java
@@ -8,13 +8,21 @@ public class CloneTest {
         PlanWork p1 = new PlanWork("1001", email);
         //浅克隆
         PlanWork p2 = p1.clone();
+        p2.setWorkName("1002");
         p2.getEmail().setContent("10:30上课");
         System.out.println(p1.toString());
+        System.out.println(p2.toString());
+
         //重置
         p1.getEmail().setContent("20:00上课");
+        System.out.println("");
+
         //深克隆
         PlanWork p3 = p1.deepClone();
+        p3.setWorkName("1003");
         p3.getEmail().setContent("19:30上课");
         System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
     }
 }
